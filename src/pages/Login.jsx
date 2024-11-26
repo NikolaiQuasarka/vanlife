2ad4816bd7465269497c3c897e77e8f6109fff1a
NikolaiQuasarka <nikolaiquasarka@gmail.com>
2024-11-26 23:08:57 +0300

    110

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react"
-import { useLoaderData, Form } from "react-router-dom"
+import { useLoaderData, Form, redirect, useActionData } from "react-router-dom"
 import { loginUser } from "../api"
+import { setLoggedIn } from "../utils"
 
 export async function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
 }
 export async function action({ request }) {
     const formData = await request.formData()
-    console.log(formData.get("email"), formData.get("password"))
-    return null
+    const email = formData.get("email")
+    const password = formData.get("password")
+    try {
+        const data = await loginUser({ password, email })
+        console.log(data)
+        setLoggedIn(true)
+
+        const rediretTo = redirect("/host")
+        rediretTo.body = true
+        return rediretTo
+    } catch (err) {
+        console.error(err)
+        return err.message
+    }
 }
 
 export default function Login() {
     const message = useLoaderData()
     const messageElement = message ? <h2>{message}</h2> : null
 
-    const [loginStatus, setLoginStatus] = useState("idle")
-    const [loginErr, setLoginErr] = useState(null)
+    //const [loginStatus, setLoginStatus] = useState("idle")
+    //const [loginErr, setLoginErr] = useState(null)
 
-    const [userData, setUserData] = useState({
+    /* const [userData, setUserData] = useState({
         email: "",
         password: "",
     })
@@ -41,13 +54,18 @@ export default function Login() {
         } finally {
             setLoginStatus("idle")
         }
-    }
+    } */
+
+    const errorMessage = useActionData()
     return (
         <main className="login">
             <h1>Sign in your account</h1>
             {messageElement}
-            {loginErr?.message && <h2>{loginErr?.message || null}</h2>}
-            <Form method="POST">
+            {
+                //loginErr?.message && <h2>{loginErr?.message || null}</h2>
+            }
+            {errorMessage && <h2>{errorMessage}</h2>}
+            <Form method="POST" replace>
                 <input
                     type="email"
                     name="email"
@@ -63,7 +81,7 @@ export default function Login() {
                 <input
                     type="submit"
                     value="Log in"
-                    disabled={loginStatus === "submitting" ? true : false}
+                    //disabled={loginStatus === "submitting" ? true : false}
                 />
             </Form>
         </main>
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
 import { redirect } from "react-router-dom"
 
 export async function isAuthorized() {
-    const isLoggedIn = false
+    let isLoggedIn
+    const loggedin = localStorage.getItem("loggedin")
+
+    if (loggedin === null) isLoggedIn = false
+    else isLoggedIn = true
 
     if (!isLoggedIn) {
         const message = "You must Log in first"
@@ -10,4 +14,7 @@ export async function isAuthorized() {
         throw response
     }
     return null
+}
+export function setLoggedIn(value) {
+    localStorage.setItem("loggedin", value)
 }
