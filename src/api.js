09b43321b26b4307385d35eb90326ce63961ddf2
NikolaiQuasarka <nikolaiquasarka@gmail.com>
2024-11-30 23:21:23 +0300

    127

--- src/api.js
+++ src/api.js
@@ -1,4 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAT8z4mVhES0YTep5WkBDXpuqAj_78AVMw",
+    authDomain: "vanlife-a1990.firebaseapp.com",
+    projectId: "vanlife-a1990",
+    storageBucket: "vanlife-a1990.firebasestorage.app",
+    messagingSenderId: "501213988530",
+    appId: "1:501213988530:web:930b368dc4a5caa661abab",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
 export async function getVans() {
+    const querySnapshot = await getDocs(vansCollectionRef)
+    const dataArray = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+    }))
+    console.log(dataArray)
+    return dataArray
+}
+/*export async function getVans() {
     const res = await fetch("/api/vans")
     if (!res.ok) {
         throw {
@@ -10,6 +41,7 @@ export async function getVans() {
     const data = await res.json()
     return data.vans
 }
+*/
 
 export async function getVan(id) {
     const res = await fetch(`/api/vans/${id}`)
